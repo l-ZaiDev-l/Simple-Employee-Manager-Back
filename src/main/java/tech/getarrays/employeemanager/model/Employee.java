48077e9f3819801235b227b7038be5bb99038232
ZaidDev <zaidhankri2@gmail.com>
2024-07-14 22:04:08 +0100

    first commit

---
+++ src/main/java/tech/getarrays/employeemanager/model/Employee.java
@@ -0,0 +1,28 @@
+package tech.getarrays.employeemanager.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageUrl;
+    @Column(nullable = false, updatable = false)
+    private String employeeCode;
+}
